b71bc6a4ebd507a8b539ab8390618ab6d100c6ba
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-25 12:19:32 -0800

    Banana: Import statements cleanup

--- src/main/java/tech/aroma/banana/client/Banana.java
+++ src/main/java/tech/aroma/banana/client/Banana.java
@@ -36,7 +36,6 @@ import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
 import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
 import static tech.sirwellington.alchemy.arguments.assertions.NetworkAssertions.validPort;
 import static tech.sirwellington.alchemy.arguments.assertions.StringAssertions.nonEmptyString;
-import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
 
 /**
  *
