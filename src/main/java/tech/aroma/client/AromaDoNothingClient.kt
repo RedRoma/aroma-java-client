557f9ea019dbd6774558c8b35a9aba7694ede784
Wellington Moreno <jwellington.moreno@gmail.com>
2017-06-03 10:32:19 -0700

    Rewrites AromaDoNothingClient

---
+++ src/main/java/tech/aroma/client/AromaDoNothingClient.kt
@@ -0,0 +1,55 @@
+package tech.aroma.client
+
+import tech.aroma.client.exceptions.AromaException
+import tech.sirwellington.alchemy.annotations.access.Internal
+import tech.sirwellington.alchemy.annotations.designs.patterns.SingletonPattern
+
+/**
+ * Created by Commander on 4/1/2017.
+ */
+@Internal
+@SingletonPattern
+internal class AromaDoNothingClient private constructor() : Aroma
+{
+
+    override fun begin(): Aroma.Request
+    {
+        return RequestDoNothing
+    }
+
+    /**
+     * This inner class is a front for a [Request object][Request] that does absolutely nothing.
+     */
+    @Internal
+    @SingletonPattern
+    private object RequestDoNothing : Aroma.Request
+    {
+
+        override fun withBody(message: String, vararg args: Any): Aroma.Request
+        {
+            return this
+        }
+
+        override fun titled(title: String): Aroma.Request
+        {
+            return this
+        }
+
+        @Throws(IllegalArgumentException::class)
+        override fun withPriority(priority: Priority): Aroma.Request
+        {
+            return this
+        }
+
+        @Throws(IllegalArgumentException::class, AromaException::class)
+        override fun send()
+        {
+
+        }
+    }
+
+    companion object
+    {
+        val INSTANCE = AromaDoNothingClient()
+    }
+}
