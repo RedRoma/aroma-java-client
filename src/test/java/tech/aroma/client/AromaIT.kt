f0307281a3b066a377b88f1d65fde73dac1277fc
Wellington Moreno <wellington@censio.love>
2018-09-28 11:46:44 -0700

    Updates copyright

--- src/test/java/tech/aroma/client/AromaIT.kt
+++ src/test/java/tech/aroma/client/AromaIT.kt
@@ -1,13 +1,11 @@
-package tech.aroma.client
-
 /*
- * Copyright 2017 RedRoma, Inc.
+ * Copyright 2018 RedRoma, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may not use this file except in compliance with the License.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +13,9 @@ package tech.aroma.client
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package tech.aroma.client
+
 
 import com.google.common.util.concurrent.MoreExecutors
 import org.junit.Before
